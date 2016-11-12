3c7084f1ee50de16c50f6c6c0293b569b99c7625
John Doe <webdevnorth@gmail.com>
2016-11-12 15:39:00 +0000

    implemented basic styling for xl-screens

--- app/containers/QuoteContainer.js
+++ app/containers/QuoteContainer.js
@@ -4,10 +4,8 @@ var Quote = require('../components/Quote.js');
 var QuoteContainer = React.createClass({
 	render: function() {
 		return (
-			<div class="row">
-				<div class="col-xs-12">
-					<Quote />
-				</div>
+			<div className="row">				
+				<Quote />				
 			</div>
 		)
 	}
