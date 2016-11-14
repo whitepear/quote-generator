403cfcb3a15fe852ee377d4772353c12ac142ba6
John Doe <webdevnorth@gmail.com>
2016-11-14 01:53:20 +0000

    implemented quoteFetcher within QuoteContainer

--- app/containers/QuoteContainer.js
+++ app/containers/QuoteContainer.js
@@ -1,28 +1,46 @@
 var React = require('react');
+// Components
 var Title = require('../components/Title.js');
 var Loading = require('../components/Loading.js');
 var QuoteText = require('../components/QuoteText.js');
 var PastQuotes = require('../components/PastQuotes.js');
 var QuoteButtons = require('../components/QuoteButtons.js');
+// Utils
+var quoteFetcher = require('../utils/quoteFetcher.js');
 
 var QuoteContainer = React.createClass({
 	getInitialState: function() {
 		return {
-			loading: true
+			loading: true,
+			quote: {},
+			error: false
 		}
 	},
+	componentDidMount: function() {
+		this.fetchQuote();
+	},
+	fetchQuote: function() {
+		quoteFetcher()
+		.then(function(quoteData) {
+			this.setState({
+				loading: false,
+				quote: quoteData
+			});
+		}.bind(this))
+		.catch(function() {
+			this.setState({
+				loading: false,
+				error: true
+			});
+		}.bind(this));
+	},
 	render: function() {
-		if (this.state.loading) {
-			var quoteBoxContent = <Loading />;
-		} else {
-			quoteBoxContent = <QuoteText />;
-		}
-
+		console.log(this.state);
 		return (
 			<div className="row">				
 				<div className="col-xs-12">
 					<Title />					
-					{quoteBoxContent}					
+					{this.state.loading ? <Loading /> : <QuoteText quoteData={this.state.quote}/>}					
 					<PastQuotes />
 					<QuoteButtons />			
 				</div>
