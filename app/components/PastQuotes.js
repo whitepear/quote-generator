1cce0a48529116065e8d8cb331d19d725eb4e1e6
John Doe <webdevnorth@gmail.com>
2016-11-19 22:39:02 +0000

    generated quotes hidden initially, app is styled for xl-screens

--- app/components/PastQuotes.js
+++ app/components/PastQuotes.js
@@ -4,7 +4,7 @@ function PastQuotes(props) {
 	var key = 0;
 	return (
 		<div className="past-quotes col-sm-6 hidden-xs">
-			<h1>Generated Quotes</h1>
+			{props.pastQuotes.length > 0 ? <h1>Generated Quotes</h1> : <h1></h1>}
 			{props.pastQuotes.map(function(pastQuote) {
 				return <div key={key++}>{pastQuote}</div>
 			})}
