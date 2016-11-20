0ce459bfcf30ad48c6f34e9547a443d10330b577
John Doe <webdevnorth@gmail.com>
2016-11-20 23:55:18 +0000

    removed url-shortener, twitter will always default any link to 23 character-length

--- app/utils/shortenUrl.js
+++
@@ -1,8 +0,0 @@
-var axios = require('axios');
-
-function shortenUrl(url) {
-	var fullUrl = 'https://api-ssl.bitly.com/v3/shorten?access_token=d1336ada598bd70570ff94eaa75264ecd8d4da98&longUrl=' + url;
-	return axios.get(fullUrl);
-}
-
-module.exports = shortenUrl;
