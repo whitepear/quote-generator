556ef718511c7fd130be804dd1116707b4b6c77c
John Doe <webdevnorth@gmail.com>
2016-12-05 17:36:31 +0000

    refactored file structure to include server directory, changed webpack config to match; replaced autoprefixer loader with postcss loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-	template: __dirname + '/app/index.html',
+	template: __dirname + '/client/index.html',
 	filename: 'index.html',
 	inject: 'body'
 });
 
 module.exports = {
 	entry: [
-		'./app/index.js'
+		'./client/index.js'
 	],
 	output: {
-		path: __dirname + '/dist',
+		path: __dirname + '/server/public',
 		filename: 'bundle.js'
 	},
 	module: {
 		loaders: [
 			{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-			{test: /\.scss$/, exclude: /node_modules/, loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'},
+			{test: /\.scss$/, exclude: /node_modules/, loader: 'style-loader!css-loader!postcss-loader!sass-loader'},
 			{test: /\.(png|jpg|otf|ttf)$/, exclude: /node_modules/, loader: 'url-loader?limit=10000'}
 		]
 	},
